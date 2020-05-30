eb93c12803bc81f87ac5802bf3f92b717f0d1970
SaukVA <kowsva16@gmail.com>
2020-05-30 18:42:16 +0200

    Terminada Practica opción 5p

---
+++ P5/Entrega/comun.h
@@ -0,0 +1,40 @@
+typedef struct {
+   char *lexema;
+   int nlin,ncol;
+   int tipo;
+   string th, cod;
+   int dir;
+} MITIPO;
+
+#define YYSTYPE MITIPO
+
+const int ERRLEXICO=1,
+          ERRSINT=2,
+          ERREOF=3,
+          ERRLEXEOF=4,
+          
+          ERR_YADECL=5,
+          ERR_NODECL=6,
+          ERR_NOCABE=7,
+          ERR_MAXTEMP=8,
+          
+          ERR_RANGO=9,
+          ERR_IFWHILE=10,
+          
+          ERR_TOCHR=11,
+          
+          ERR_FALTAN=12,
+          ERR_SOBRAN=13,
+          ERR_INDICE_ENTERO=14,
+          
+          ERR_ASIG=15,
+          
+          ERR_OPIZQ=16,
+          ERR_OPDER=17;
+
+
+
+void errorSemantico(int nerror,int fila,int columna,const char *s);
+void msgErrorOperador(int tipoesp,const char *op,int linea,int columna,int lado);
+void msgError(int nerror,int nlin,int ncol,const char *s);
+
